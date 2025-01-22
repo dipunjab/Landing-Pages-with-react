9291d22ca7afdd28bc41725e88ad4c1c44b16a9a
Usman <ghaniusman0287@gmail.com>
2025-01-22 22:52:35 +0500

    3d macbook with threej/and others

--- 3dapple landing page/src/App.jsx
+++ 3dapple landing page/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import "./style.css"
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
+import MacContainer from './MacContainer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-screen font-["Helvetica_Now_Display"]'>
+      <div className='navbar flex gap-10 py-10 absolute top-0 left-1/2 -translate-x-1/2'>
+        {["iphone","iPad", "services","mac", "products"].map(e => <a href='' className='text-white font-[500] text-md'>{e}</a>)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='absolute flex flex-col items-center text-white top-24 left-1/2 -translate-x-1/2 '>
+        <h3 className='masked text-6xl tracking-tighter font-[700]'>macbook pro.</h3>
+        <h4>Oh pro!</h4>
+        <p className='text-center w-3/4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam repellat, esse quibusdam illo</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Canvas camera={{ fov: 20, position: [0, -10, 220] }}>
+      {/* <OrbitControls/> */}
+      <Environment
+        files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/studio_small_09_2k.hdr",]} />
+      <ScrollControls pages={3}>
+        <MacContainer />
+      </ScrollControls>
+    </Canvas>
+    </div>
   )
 }
 
